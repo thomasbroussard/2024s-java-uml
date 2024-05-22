7953ec3777426df7affe6bbe40dd28f86f7a6baa
Présentation <tbr@tbr.com>
2024-05-22 15:22:57 +0200

    exam training

--- exam-training/src/main/java/fr/epita/exam/launch/Launcher.java
+++ exam-training/src/main/java/fr/epita/exam/launch/Launcher.java
@@ -3,6 +3,6 @@ package fr.epita.exam.launch;
 public class Launcher {
 
     public static void main(String[] args) {
-
+        TestDMO2.test();
     }
 }
---
+++ exam-training/src/main/java/fr/epita/exam/launch/TestDMO2.java
@@ -0,0 +1,10 @@
+package fr.epita.exam.launch;
+
+import fr.epita.exam.datamodel.Passenger;
+
+public class TestDMO2 {
+
+    public static void test(){
+        System.out.println(new Passenger("Abbing, Mr Anthony","3rd", 20.0, "male", true ));
+    }
+}
